3a7be2accdb23aa165eb38a83f6b78425ce5d676
Sun Qin <sqlxx@163.com>
2025-11-06 17:55:17 +0800

    add softmax

--- softmax.py
+++ softmax.py
@@ -1,8 +1,73 @@
+from matplotlib import spines
+import test
 import torch
 import torchvision
 from torch.utils import data
 from torchvision import transforms
 import matplotlib.pyplot as plt
+from torch import nn
+
+class Accumulator:
+    def __init__(self, n):
+        self.data = [0.0] * n
+    
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class Animator:
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None, ylim=None, xscale='linear', yscale='linear', fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1, figsize=(3.5, 2.5)):
+        plt.ion()
+        if legend is None:
+            legend = []
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        
+        self.config_axes = lambda: self.set_axes(self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def set_axes(self, axes, xlabel, ylabel, xlim, ylim, xscale='linear', yscale='linear', legend=None, grid=True):
+        axes.set_xlabel(xlabel)
+        axes.set_ylabel(ylabel)
+        axes.set_xscale(xscale)
+        axes.set_yscale(yscale)
+        axes.set_xlim(xlim)
+        axes.set_ylim(ylim)
+        if legend:
+            axes.legend(legend)
+        if grid:
+            axes.grid(True, linestyle='--', alpha=0.5)
+        axes.spines['right'].set_visible(False)
+        axes.spines['top'].set_visible(False)
+    
+    def add(self, x, y):
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x]*n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        # plt.draw()
+        self.fig.canvas.draw()
+        plt.pause(0.1)
 
 def get_fashion_mnist_labels(labels):
     """返回Fashion-MNIST数据集的文本标签"""
@@ -11,6 +76,7 @@ def get_fashion_mnist_labels(labels):
     return [text_labels[int(i)] for i in labels]
 
 def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    print("show image")
 
     figsize = (num_cols * scale, num_rows * scale)
     _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
@@ -42,6 +108,79 @@ def load_data_fashion_mnist(batch_size, resize=None):
     return (data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=4),
             data.DataLoader(mnist_test, batch_size, shuffle=False, num_workers=4))
 
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return X_exp / partition
+
+def net(X):
+    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat[range(len(y_hat)), y])
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+def train_epoch(net, train_iter, loss, updater):
+    if isinstance(net, torch.nn.Module):
+        net.train()
+
+    # training loss, accuracy, number of examples
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        ll = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            ll.mean().backward()
+            updater.step()
+        else: # 使用自制的优化器和损失函数
+            ll.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(ll.sum()), accuracy(y_hat, y), y.numel())
+    
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+def train(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    print(f'train loss {train_loss:.3f}, train acc {train_acc:.3f}, test acc {test_acc:.3f}')
+
+def sgd(params, lr, batch_size):
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad/batch_size
+            param.grad.zero_()
+
+def updater(batch_size):
+    return sgd([W, b], 0.1, batch_size)
+
+def predict(net, test_iter, n=6):
+    for X, y in test_iter:
+        break
+    trues = get_fashion_mnist_labels(y)
+    preds = get_fashion_mnist_labels(net(X).argmax(axis=1))
+    titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
+    show_images(X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+
 if __name__ == "__main__":
     train_iter, test_iter = load_data_fashion_mnist(256)
 
@@ -49,4 +188,26 @@ if __name__ == "__main__":
         print(X.shape, X.dtype, y.shape, y.dtype)
         break
 
+    num_inputs = 784
+    num_outputs = 10
+    W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+    b = torch.zeros(num_outputs, requires_grad=True)
+
+    num_epochs = 10
+    # train(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+
+    nn_net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
+    def init_weights(m):
+        if type(m) is nn.Linear:
+            nn.init.normal_(m.weight, std=0.01)
+    nn_net.apply(init_weights)
+    loss = nn.CrossEntropyLoss(reduction='none')
+    trainer = torch.optim.SGD(nn_net.parameters(), lr=0.1)
+    train(nn_net, train_iter, test_iter, loss, num_epochs, trainer)
+    
+
+    plt.ioff()
+    # predict(nn_net, test_iter)
+    
+    # plt.show()
 
